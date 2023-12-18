ba8937a4348a52de9b25dd336d4f36240af718bf
Julio Pazini <juliopazini@gmail.com>
2023-12-18 16:19:17 +0100

    Add Output path input

--- jira_sprint.py
+++ jira_sprint.py
@@ -1,6 +1,5 @@
 import pandas as pd
-import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox, filedialog
 import requests
 import json
 import csv
@@ -8,13 +7,23 @@ from ttkbootstrap.constants import *
 import ttkbootstrap as tb
 import os
 
-CONFIG_FILE = "config.json"
+base_dir = os.path.dirname(__file__)
+CONFIG_FILE = os.path.join(base_dir, './config.json')
 
 
 def process_excel_file():
     try:
+        with open(CONFIG_FILE, "r") as file:
+            config_data = json.load(file)
+    except FileNotFoundError:
+        config_data = {"username": "", "api_token": "", "output_file": ""}
+
+    try:
+        sprint_data_file_name = 'sprint_data.csv'
+        sprint_data_file_path = os.path.join(os.path.dirname(__file__), sprint_data_file_name)
+
         # Read the existing CSV file
-        df = pd.read_csv('sprint_data.csv')
+        df = pd.read_csv(sprint_data_file_path)
 
         # Determine the 'Department' based on the 'Reporter' column
         conditions = [
@@ -36,13 +45,16 @@ def process_excel_file():
         selected_columns = ['Issue key', 'Summary', 'Reporter', 'Department', 'Theme']
         df = df[selected_columns]
 
+        sprint_id = sprint_entry.get()
+        output_file_name = f'sprint_{sprint_id}.xlsx'
+        output_file_path = os.path.join(config_data["output_file"], output_file_name)
+
         # Create a new Excel workbook and add a worksheet
-        excel_file_path = 'output.xlsx'
-        with pd.ExcelWriter(excel_file_path, engine='xlsxwriter') as writer:
+        with pd.ExcelWriter(output_file_path, engine='xlsxwriter') as writer:
             create_excel_file(df, writer)
 
-        result_label.config(text="File saved to 'output.xlsx'", font=('Helvetica', 14), bootstyle='success')
-        delete_file('sprint_data.csv')
+        result_label.config(text=f"File saved to \n {output_file_path}", font=('Helvetica', 14), bootstyle='success')
+        delete_file(sprint_data_file_path)
     except FileNotFoundError:
         result_label.config(text="File not found.", font=('Helvetica', 14), bootstyle='danger')
     except Exception as e:
@@ -89,7 +101,7 @@ def get_tickets():
         with open(CONFIG_FILE, "r") as file:
             config_data = json.load(file)
     except FileNotFoundError:
-        config_data = {"username": "", "api_token": ""}
+        config_data = {"username": "", "api_token": "", "output_file": ""}
 
     # Replace with your Jira details
     jira_url = 'https://kaferocks.atlassian.net'
@@ -104,33 +116,39 @@ def get_tickets():
 
     # Check for a successful response (status code 200)
     if response.status_code == 200:
-        issues = response.json()['issues']
-
-        # Filter issues based on status
-        filtered_issues = [issue for issue in issues if
-                        issue['fields']['status']['name'] in ['In Staging', 'Approved by Tech', 'In Progress']]
-
-        # Save filtered issues to a CSV file
-        with open('sprint_data.csv', 'w', newline='') as csvfile:
-            fieldnames = ['Issue key', 'Summary', 'Reporter', 'Status']  # Adjust as needed
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-            writer.writeheader()
-            for issue in filtered_issues:
-                writer.writerow({
-                    'Issue key': issue['key'],
-                    'Summary': issue['fields']['summary'],
-                    'Reporter': issue['fields']['reporter']['displayName'],
-                    'Status': issue['fields']['status']['name']
-                    # Add more fields as needed
-                })
-
-        print('Filtered data exported successfully.')
-        result_label.config(text="Tickets retrieved!")
+        filter_excel_file(response)
     else:
         result_label.config(text="Empty fields..")
         print(f'Error: {response.status_code}')
         print(response.text)
+
+
+def filter_excel_file(response):
+    issues = response.json()['issues']
+
+    # Filter issues based on status
+    filtered_issues = [issue for issue in issues if
+                    issue['fields']['status']['name'] in ['In Staging', 'Approved by Tech', 'In Progress']]
+
+    file_path = os.path.join(os.path.dirname(__file__), 'sprint_data.csv')
+
+    # Save filtered issues to a CSV file
+    with open(file_path, 'w', newline='') as csvfile:
+        fieldnames = ['Issue key', 'Summary', 'Reporter', 'Status']  # Adjust as needed
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        for issue in filtered_issues:
+            writer.writerow({
+                'Issue key': issue['key'],
+                'Summary': issue['fields']['summary'],
+                'Reporter': issue['fields']['reporter']['displayName'],
+                'Status': issue['fields']['status']['name']
+                # Add more fields as needed
+            })
+
+    print('Filtered data exported successfully.')
+    result_label.config(text="Tickets retrieved!")
 
 
 def process_sprint():
@@ -139,16 +157,17 @@ def process_sprint():
 
 
 def open_config_window():
+    global output_entry
     config_window = tb.Toplevel(root)
     config_window.title("Config")
-    config_window.geometry("400x300")
+    config_window.geometry("400x350")
 
     # Load existing configuration from JSON file
     try:
         with open(CONFIG_FILE, "r") as file:
             config_data = json.load(file)
     except FileNotFoundError:
-        config_data = {"username": "", "api_token": ""}
+        config_data = {"username": "", "api_token": "", "output_file": ""}
 
     username_label = tb.Label(config_window, text=" Jira Username:", font=('Helvetica', 12), bootstyle='default')
     username_label.pack(pady=5)
@@ -164,11 +183,24 @@ def open_config_window():
     token_entry.pack(pady=5)
     token_entry.insert(0, config_data["api_token"])
 
+    # Output File
+    output_label = tb.Label(config_window, text="Output File:", font=('Helvetica', 12), bootstyle='default')
+    output_label.pack(pady=5)
+
+    output_entry = tb.Entry(config_window, width=30, font=('Helvetica', 12), bootstyle='info')
+    output_entry.pack(pady=5)
+    output_entry.insert(0, config_data["output_file"])
+
+    # Select Folder Button
+    select_folder_button = tb.Button(config_window, text="Select Folder", bootstyle='info', command=select_output_folder)
+    select_folder_button.pack(pady=5)
+
     def save_config():
         # Save configuration to JSON file
         new_config_data = {
             "username": username_entry.get(),
-            "api_token": token_entry.get()
+            "api_token": token_entry.get(),
+            "output_file": output_entry.get()
         }
 
         with open(CONFIG_FILE, "w") as file:
@@ -177,8 +209,14 @@ def open_config_window():
         messagebox.showinfo("Config Saved", "Configuration saved successfully.")
         config_window.destroy()
 
-    save_button = tb.Button(config_window, text="Save", bootstyle='success-outline',  command=save_config)
-    save_button.pack(pady=25)
+    save_button = tb.Button(config_window, text="Save", bootstyle='success-outline', command=save_config)
+    save_button.pack(side=tb.BOTTOM, pady=15)
+
+
+def select_output_folder():
+    folder_selected = filedialog.askdirectory()
+    output_entry.delete(0, tb.END)
+    output_entry.insert(0, folder_selected)
 
 
 def delete_file(file_path):
@@ -195,8 +233,8 @@ root = tb.Window(themename='journal')
 
 # Main window
 root.title("Sprint Processor")
-root.iconbitmap('icon.png')
-root.geometry("400x300")
+root.geometry("400x350")
+root.resizable(False, False)
 
 sprint_label = tb.Label(root, text="Jira Sprint ID:", font=('Helvetica', 14), bootstyle='default')
 sprint_label.pack(pady=10)
@@ -211,7 +249,7 @@ result_label = tb.Label(root, text="", font=('Helvetica', 12))
 result_label.pack(pady=10)
 
 config_button = tb.Button(root, text="Config.", bootstyle='secondary-outline', command=open_config_window)
-config_button.pack(pady=30)
+config_button.pack(side=tb.BOTTOM, pady=15)
 
 # Run the GUI
 root.mainloop()
